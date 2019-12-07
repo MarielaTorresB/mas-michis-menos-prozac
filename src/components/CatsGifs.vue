698932f3ac627cc51c920718828ff743d71a153e
MarielaTorresB <martz_maru@hotmail.com>
2019-12-07 01:43:05 -0600

    Utilizando axios

--- src/components/CatsGifs.vue
+++ src/components/CatsGifs.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
-        <h1> Fabulosos gifs de gatos WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1> {{info}} </h1>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "CatsGifs",
+    data() {
+        return {
+            info:"",
+            URL_BASE:"http://api.giphy.com/v1/gifs/search?q=",
+            LIMIT:"&limit=5"
+        }
+    },
+    created() {
+    axios
+      .get(`${this.URL_BASE}cats${process.env.API_GIPHY}${this.LIMIT}`)
+      .then(response => {
+        this.info = response.data.data;
+      })
+
+      .catch();
+  },
 }
+
 </script>

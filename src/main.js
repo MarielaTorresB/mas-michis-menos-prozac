6ffed38249754db49cdcbc0965cd140cff0158a4
MarielaTorresB <martz_maru@hotmail.com>
2019-12-06 18:06:25 -0600

    firebase instalado

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import router from "./router";
 import store from "./store";
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ1JaixEnXr2T9xqs-l42iM1eCN2sxfHI",
+  authDomain: "michis-df301.firebaseapp.com",
+  databaseURL: "https://michis-df301.firebaseio.com",
+  projectId: "michis-df301",
+  storageBucket: "michis-df301.appspot.com",
+  messagingSenderId: "255751988604",
+  appId: "1:255751988604:web:24d7df8f02b0e71553b0f9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.use(Buefy)
 
---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login" id="firebaseui-auth-container">
+        
+    </div>
+</template>
+
+<script>
+import firebase from "firebase";
+import firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+
+export default {
+    name: "Login",
+    mounted() {
+        var ui = new firebaseui.auth.AuthUI(firebase.auth());
+        var uiConfig = {
+            signInSuccessUrl: "/about",
+            signInOptions: [firebase.auth.FacebookAuthProvider.PROVIDER_ID]
+        };
+        ui.start("#firebaseui-auth-container", uiConfig);
+    }
+}
+
+
+</script>

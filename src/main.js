fd9f98e9badb967dc08a4210288e8b7181cc4954
MarielaTorresB <martz_maru@hotmail.com>
2019-12-08 01:29:47 -0600

    Intentando arreglar problema con Api de firebase, quitando comillas en el main

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import firebase from 'firebase';
 
 
 const firebaseConfig = {
-  apiKey: "process.env.VUE_APP_API_KEY",
+  apiKey: process.env.VUE_APP_API_KEY,
   authDomain: "michis-df301.firebaseapp.com",
   databaseURL: "https://michis-df301.firebaseio.com",
   projectId: "michis-df301",

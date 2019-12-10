3b33ee06beeff12e36564f599290b57fb3743b75
MarielaTorresB <martz_maru@hotmail.com>
2019-12-10 03:18:34 -0600

    Instalada la libreria de persistencia de datos para vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
+// plugin a agregar
+import  createPersistedState  from  'vuex-persistedstate';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    favGifs:[]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  // aquí agregamos el plugin
+	plugins: [createPersistedState()]
 });
